217b407a380531361ee93d75c988ac64a3263f88
Artem Sokolov <artem.sokolov.dev@gmail.com>
2026-04-03 12:23:42 -0400

    rename: stm_client → auth_client

--- core/client.py
+++ core/client.py
@@ -3,5 +3,5 @@ import httpx
 from core.config import settings
 
 
-def stm_client() -> httpx.AsyncClient:
+def auth_client() -> httpx.AsyncClient:
     return httpx.AsyncClient(headers={"apikey": settings.token.get_secret_value()})
